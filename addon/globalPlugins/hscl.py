7b59a079a3eadad0b36067bc4d734ca70178a4df
Samuel Kacer <samuel.kacer@gmail.com>
2021-08-23 12:11:25 +0100

    Get selected text

---
+++ addon/globalPlugins/hscl.py
@@ -0,0 +1,48 @@
+# Hearthstone card lookup
+# Copyright (C) 2021 Samuel Kacer
+#GNU GENERAL PUBLIC LICENSE V2
+# author: Samuel Kacer <samuel.kacer@gmail.com>
+# <home page>
+
+import api
+import globalPluginHandler
+from scriptHandler import script
+from textInfos import POSITION_SELECTION
+from ui import browseableMessage
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	@script("Lookup Hearthstone card info.", gesture="kb:NVDA+H")
+	def script_lookup(self, gesture):
+		selection = getSelectedText()
+		if selection:
+			browseableMessage(selection)	
+
+# from quick dictionary addon by 
+def getSelectedText() -> str:
+	"""Retrieve the selected text.
+	If the selected text is missing - extract the text from the clipboard.
+	If the clipboard is empty or contains no text data - announce a warning.
+	@return: selected text, text from the clipboard, or an empty string
+	@rtype: str
+	"""
+	obj = api.getFocusObject()
+	treeInterceptor = obj.treeInterceptor
+	if hasattr(treeInterceptor, 'TextInfo') and not treeInterceptor.passThrough:
+		obj = treeInterceptor
+	try:
+		info = obj.makeTextInfo(POSITION_SELECTION)
+	except (RuntimeError, NotImplementedError):
+		info = None
+	if not info or info.isCollapsed:
+		try:
+			text = api.getClipData()
+		except Exception:
+			text = ''
+		if not text or not isinstance(text, str):
+			# Translators: User has pressed the shortcut key for translating selected text,
+			# but no text was actually selected and clipboard is clear
+			ui.message(_("There is no selected text, the clipboard is also empty, or its content is not text!"))
+			return ''
+		return text
+	return info.text
+
