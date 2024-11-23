a65bd2d08914463e195fad45183429e976e0a877
Samuel Kacer <samuel.kacer@gmail.com>
2024-11-23 12:27:22 +0000

    Transform addon into python module

---
+++ addon/globalPlugins/hscl/__init__.py
@@ -0,0 +1,8 @@
+# Hearthstone card lookup
+# Copyright (C) 2021 Samuel Kacer
+#GNU GENERAL PUBLIC LICENSE V2
+# author: Samuel Kacer <samuel.kacer@gmail.com>
+# https://github.com/SamKacer/HearthstoneCardLookup
+from . import hscl
+
+GlobalPlugin = hscl.GlobalPlugin
