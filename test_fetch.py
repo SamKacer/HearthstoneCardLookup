f07f700684faa565c7100c056b4124474bea4534
Samuel Kacer <samuel.kacer@gmail.com>
2024-11-24 13:11:35 +0000

    Finish first fetch test

--- test_fetch.py
+++ test_fetch.py
@@ -1,7 +1,7 @@
 from difflib import ndiff
 from addon.globalPlugins.hscl import fetch
 
-angryChickenText = """angry chicken
+angryChickenTextLines = """angry chicken
 1 mana
 1 1
 Has +5 Attack while damaged.
@@ -10,6 +10,14 @@ Minion
 Neutral
 Rare
 Legacy
-There is no beast more frightening (or ridiculous) than a fully enraged chicken."""
+<i>There is no beast more frightening (or ridiculous) than a fully enraged chicken.</i>""".split('\n')
+
 def test_fetchMinion():
-	pass
+	cardTextResult = fetch.getCardFieldsIterator('angry chicken')
+	if isinstance(cardTextResult, str):
+		raise Exception(f"Failed to fetch card text: {cardTextResult}")
+	else:
+		cardTextLines = list(cardTextResult)
+		if cardTextLines != angryChickenTextLines:
+			diff = '\n'.join(ndiff(angryChickenTextLines, cardTextLines))
+			raise Exception(f"Card text did not match:\n{diff}")
