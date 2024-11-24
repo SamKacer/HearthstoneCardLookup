73bc75bd371685bb1beb4cfc81dc4dd855938ef6
Samuel Kacer <samuel.kacer@gmail.com>
2024-11-24 12:41:28 +0000

    Add basic test skeleton

---
+++ test_fetch.py
@@ -0,0 +1,15 @@
+from difflib import ndiff
+from addon.globalPlugins.hscl import fetch
+
+angryChickenText = """angry chicken
+1 mana
+1 1
+Has +5 Attack while damaged.
+Beast
+Minion
+Neutral
+Rare
+Legacy
+There is no beast more frightening (or ridiculous) than a fully enraged chicken."""
+def test_fetchMinion():
+	pass
